70d2de46640422f1c187b7aa894748a92ccd73c8
iSO <iso@thinktux.net>
2025-03-30 22:08:52 +0000

    rubiks: some manual tries

--- example_rubiks_cube/src/main.rs
+++ example_rubiks_cube/src/main.rs
@@ -1,47 +1,63 @@
-use example_rubiks_cube::{solve_cube, state::{transition::{Axis::*, Rotation, Times::*}, LineIndex::*}, RubiksCube};
+use example_rubiks_cube::{solve_cube, solve_cube_with_transitions, state::{transition::{Axis::*, Rotation, Times::*}, LineIndex::*}, RubiksCube};
 use meet_in_the_middle::State;
 
 fn main() {
     // 0: white,
-    // 1: yellow,
-    // 2: red,
-    // 3: green,
-    // 4: orange,
-    // 5: blue
+    // 1: orange,
+    // 2: green,
+    // 3: red,
+    // 4: blue,
+    // 5: yellow
 
-    // let cube = RubiksCube::from_unvalidated_raw_colors(&[
-    //    [0, 1, 0, 2, 0, 3, 0, 4, 4],
-    //    [3, 5, 3, 1, 4, 4, 2, 5, 3],
-    //    [2, 0, 5, 3, 3, 1, 2, 0, 0],
-    //    [1, 0, 5, 2, 2, 3, 5, 4, 4],
-    //    [4, 4, 4, 2, 5, 5, 3, 3, 1],
-    //    [1, 2, 2, 0, 1, 5, 5, 1, 1],
+    let cube = RubiksCube::from_unvalidated_raw_colors(&[
+        [4, 0, 1, 1, 0, 5, 2, 4, 2],
+        [1, 2, 3, 1, 1, 5, 4, 3, 3],
+        [0, 3, 0, 1, 2, 0, 5, 4, 4],
+        [1, 3, 0, 1, 3, 0, 5, 0, 1],
+        [4, 2, 5, 4, 4, 4, 2, 2, 0],
+        [2, 5, 3, 2, 5, 3, 3, 5, 5]
+    ]).unwrap();
+
+    // let cube_copied = RubiksCube::from_unvalidated_raw_colors(&[
+    //    [0, 0, 0, 0, 0, 0, 1, 1, 5],
+    //    [1, 1, 5, 1, 1, 5, 1, 1, 4],
+    //    [2, 2, 3, 2, 2, 3, 5, 5, 2],
+    //    [2, 3, 3, 2, 3, 2, 1, 3, 2],
+    //    [4, 4, 4, 0, 4, 4, 0, 4, 4],
+    //    [3, 3, 0, 5, 5, 4, 5, 5, 3]
     // ]).unwrap();
 
-    // should only take 3 moves
+    // let cube = RubiksCube::solved()
+    //     .apply(&Rotation::new(Y, First, Thrice))
+    //     .apply(&Rotation::new(X, Last, Thrice))
+    //     .apply(&Rotation::new(Y, First, Once))
+    //     .apply(&Rotation::new(Z, First, Once));
+
+    // assert_eq!(cube, cube_copied);
+
     // let cube = RubiksCube::from_unvalidated_raw_colors(&[
-    //    [0, 0, 4, 0, 0, 0, 0, 0, 0],
-    //    [4, 4, 4, 4, 4, 4, 3, 3, 1],
-    //    [3, 3, 3, 3, 3, 3, 2, 2, 3],
-    //    [2, 2, 5, 2, 2, 5, 2, 2, 0],
-    //    [1, 5, 5, 1, 5, 5, 5, 4, 4],
-    //    [5, 5, 1, 1, 1, 1, 1, 1, 2],
+    //     [0, 0, 3, 0, 0, 3, 3, 0, 3],
+    //     [1, 1, 2, 2, 1, 1, 2, 1, 1],
+    //     [5, 3, 5, 2, 2, 0, 2, 2, 0],
+    //     [4, 4, 4, 2, 3, 5, 2, 3, 5],
+    //     [0, 4, 4, 4, 4, 3, 4, 4, 3],
+    //     [5, 5, 1, 5, 5, 5, 0, 1, 1]
     // ]).unwrap();
 
-    let mut cube = RubiksCube::solved();
-    cube = cube.apply(&Rotation::new(X, First, Thrice));
-    println!("{cube}");
-    cube = cube.apply(&Rotation::new(X, First, Thrice));
-    println!("{cube}");
-    cube = cube.apply(&Rotation::new(X, First, Thrice));
-    println!("{cube}");
-    cube = cube.apply(&Rotation::new(X, First, Thrice));
-    println!("{cube}");
+    // // needs 10 moves
+    // let cube = RubiksCube::from_unvalidated_raw_colors(&[
+    //     [2, 2, 2, 0, 0, 3, 0, 0, 4],
+    //     [5, 3, 1, 1, 1, 1, 1, 2, 2],
+    //     [2, 2, 1, 5, 2, 1, 0, 2, 5],
+    //     [0, 4, 5, 0, 3, 0, 3, 3, 3],
+    //     [3, 1, 1, 4, 4, 4, 0, 4, 4],
+    //     [3, 5, 4, 3, 5, 5, 5, 5, 4]
+    // ]).unwrap();
 
-    // println!("unsolved:\n{cube}");
-
-    // let solution = solve_cube(&cube);
-    // for step in solution {
-    //     println!("{step}\n----------------------------------------------------------------");
-    // }
+    let solution: Vec<_> = solve_cube_with_transitions(&cube);
+    for step in solution {
+        println!("{}\n----------------------------------------------------------------", step.from_state);
+        println!("Apply {}", step.transition);
+    }
+    println!("{}", RubiksCube::solved());
 }
